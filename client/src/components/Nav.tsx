1a81a88d91ec25a7fc847ac63d9e9f907221e0f5
William Wells <whanwells@gmail.com>
2021-08-04 12:54:27 -0500

    Add Nav component

---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "react-bootstrap";
+
+export const Nav = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="mb-4">
+      <Navbar.Brand href="#">Shopping Portal</Navbar.Brand>
+    </Navbar>
+  );
+}
