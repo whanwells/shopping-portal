764a8e6397c374ddd7eb81fe796e006391103b07
William Wells <whanwells@gmail.com>
2021-08-05 11:25:40 -0500

    Add React Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Switch, Route } from "react-router-dom";
 import { Navigation, ProtectedRoute } from "./router";
 import { LoginForm } from "./login";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navigation />
       <Switch>
         <Route path="/login">
@@ -12,7 +21,7 @@ const App = () => {
         </Route>
         <ProtectedRoute path="/">{null}</ProtectedRoute>
       </Switch>
-    </>
+    </QueryClientProvider>
   );
 };
 
