fdec9021a678920839afd4b917f274c136be5d8f
William Wells <whanwells@gmail.com>
2021-08-11 22:16:21 -0500

    Remove unused imports

--- api/src/test/java/com/example/demo/user/order/OrderTests.java
+++ api/src/test/java/com/example/demo/user/order/OrderTests.java
@@ -1,14 +1,10 @@
 package com.example.demo.user.order;
 
-import com.example.demo.user.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
