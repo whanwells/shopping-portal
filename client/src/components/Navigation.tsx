a6a3198b65cfa8eb3132455b134ebc8ace9a55b0
William Wells <whanwells@gmail.com>
2021-08-12 09:16:56 -0500

    Style navigation

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,28 +1,46 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { useToken } from "../token";
+
+const ROUTES = {
+  "/": "Shop",
+  "/cart": "Cart",
+  "/orders": "Orders",
+  "/logout": "Logout",
+};
 
 export const Navigation = () => {
   const { token } = useToken();
 
   return (
-    <nav>
-      <div>Shopping Portal</div>
-      {token && (
-        <ul>
-          <li>
-            <Link to="/">Shop</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
-        </ul>
-      )}
-    </nav>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      collapseOnSelect
+      className="mb-3"
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Shopping Portal
+        </Navbar.Brand>
+        {token && (
+          <>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+              <Nav className="ml-auto">
+                {Object.entries(ROUTES).map(([route, label]) => (
+                  <Nav.Link as={Link} to={route} href={route}>
+                    {label}
+                  </Nav.Link>
+                ))}
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        )}
+      </Container>
+    </Navbar>
   );
 };
