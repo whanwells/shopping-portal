845fd5c1489a3e58780ecad76efa44869853eeda
William Wells <whanwells@gmail.com>
2021-08-12 14:29:56 -0500

    Style spinner

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,3 +1,14 @@
 import type { VFC } from "react";
+import { Spinner as BsSpinner } from "react-bootstrap";
 
-export const Spinner: VFC = () => <div>Loading...</div>;
+export const Spinner: VFC = () => (
+  <div className="d-flex justify-content-center">
+    <BsSpinner
+      animation="border"
+      role="status"
+      style={{ width: "3em", height: "3em" }}
+    >
+      <span className="sr-only">Loading...</span>
+    </BsSpinner>
+  </div>
+);
