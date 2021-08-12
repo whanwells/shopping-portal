eeb5a587e1abb7ff0018c0a9ad86807a1f5c3dcf
William Wells <whanwells@gmail.com>
2021-08-12 16:24:02 -0500

    Style order details

--- client/src/order/OrderDetail.tsx
+++ client/src/order/OrderDetail.tsx
@@ -27,20 +27,24 @@ export const OrderDetail: VFC = () => {
 
   return (
     <>
-      <h1>Order #{data.id}</h1>
-      <div>
-        <h2>Summary</h2>
-        <div>Placed on: {data.date.substr(0, 10)}</div>
-        <div>Number of items: {data.lines.length}</div>
-        <div>Order total: ${data.total}</div>
+      <h1 className="h5 mb-3">Order #{data.id}</h1>
+      <div className="border-bottom">
+        <dl className="row">
+          <dt className="col-sm-4">Date</dt>
+          <dd className="col-sm-8">{data.date.substr(0, 10)}</dd>
+          <dt className="col-sm-4">Number of items</dt>
+          <dd className="col-sm-8">{data.lines.length}</dd>
+          <dt className="col-sm-4">Order total</dt>
+          <dd className="col-sm-8">${data.total}</dd>
+        </dl>
       </div>
-      <div>
-        <h2>Items</h2>
+      <div className="mt-4">
+        <h2 className="h6">Items in this order:</h2>
         <ul>
           {data.lines.map(({ id, product }: OrderLine) => (
             <li key={id}>
               <div>{product.name}</div>
-              <div>${product.msrp}</div>
+              <div className="font-weight-light">${product.msrp}</div>
             </li>
           ))}
         </ul>
